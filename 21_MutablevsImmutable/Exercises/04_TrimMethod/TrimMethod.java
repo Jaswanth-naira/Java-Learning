8d37fcb50d0ab21b08ace0f636ae4795a6822dd6
Jaswanth-naira <nairajaswanth@gmail.com>
2026-08-19 17:18:27 +0530

    feat:add TrimMethod

---
+++ 21_MutablevsImmutable/Exercises/04_TrimMethod/TrimMethod.java
@@ -0,0 +1,10 @@
+class TrimMethod 
+{
+    public static void main(String args[])
+    {
+        String s = " hello ";
+        s.trim();
+        System.out.println("[" + s + "]");
+        System.out.println("[" + s.trim() + "]");
+    }
+}
